875ae523e803afe20e7eec51310723c8adf78f20
Muhammadjon <user@JAVA.com>
2023-05-24 19:09:42 +0500

    Layout is ready + starting main

---
+++ lastapp/src/Components/Layout/index.jsx
@@ -0,0 +1,21 @@
+import layout from './style.css'
+function Layout(props) {
+    return ( 
+        <div>
+            <nav style={layout}>
+                <div className='left'>
+                    <a href="https://www.mail.com/"><img src="wat.png"/></a>
+                    <a href="https://github.com/"><img src="wat.png"/></a>
+                    <a href="https://ru.linkedin.com/"><img src="wat.png"/></a>
+                    <a href="https://stackoverflow.com/"><img src="wat.png"/></a>
+                </div>
+                <div className='right'>
+                    <a href="#">Projects</a>
+                    <a href="#">Contact me</a>
+                </div>
+            </nav>
+        </div>
+     );
+}
+
+export default Layout;
---
+++ lastapp/src/Components/Pages/Intro.jsx
@@ -0,0 +1,16 @@
+function Intro(props) {
+    return ( 
+        <div className="main">
+            <div className="name-side">
+                <img src="" />
+                <h6>Hello,I'm Alisher</h6>
+            </div>
+            <div className="heading">
+                <h1>Lorem, ipsum dolor</h1>
+                <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+            </div>
+        </div>
+     );
+}
+
+export default Intro;
